38e9a65136a9e45a053c32bfb948be2cfc0b74c5
Chris Infante <inf20001@byui.edu>
2023-02-02 17:26:29 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Overview
+
+This application was written as I learned about programming in C++.
+
+This is a program in C++ that manages a simple address book. The address book consists of contacts which have the following information: first name, last name, phone number, email and notes. The program provides the following functionality:
+
+- Creating a new contact and saving it to the address book
+- Displaying all contacts stored in the address book
+- Updating a contact stored in the address book
+- Deleting a contact stroed in the address book
+
+The program uses the iostream, fstream, and string libraries to handle input/output and file operations. The vector library is used to store the contacts as strings, and the contact class is used to store and manipulate the contact information. The address book data is stored in a text file named "addressbook.txt".
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+This application is written in C++ and was developed using Visual Studio Code.
+
+Visual Studio Code
+C/C++ for Visual Studio Code
+MinGW - Minimalist GNU for Windows (https://osdn.net/projects/mingw/)
+
+# Useful Websites
+
+- [Web Site Name](http://url.link.goes.here)
+- [C/C++ for Visual Studio Code](https://code.visualstudio.com/docs/languages/cpp)
+- [Geeks for Geeks](https://www.geeksforgeeks.org/)
+- [w3Schools](https://www.w3schools.com/cpp)
+- [Cplusplus.com](https://cplusplus.com/reference/stl/)
+
+# Future Work
+
+Things that I would like to to fix, improve, and add in the future:
+
+- Develop a granular search feature
+- Integrate into a web application for easy use
+- Store data in a database
---
+++ addressbook.cpp
@@ -0,0 +1,334 @@
+// Christopher Infante
+// CSE 310 - Winter 2023
+
+// This program is an Adress Book where you can add, delete, and display contacts.
+
+// include input/output stream library
+#include <iostream>
+
+// include file stream library
+#include <fstream>
+
+// include string library (needed for transform function)
+#include <bits/stdc++.h>
+
+// include the stl vector library
+#include <vector>
+
+// include the string library
+#include <string>
+
+// include the standard library
+#include <stdlib.h>
+
+using namespace std;
+
+fstream datahandler; // file pointer for Filestream Library  
+
+// define global variables
+string firstName;
+string lastName;
+string phoneNumber;
+string email;
+string notes;
+
+// define the structure of the contact class
+
+class contact {
+    public:
+          
+    // creates a new contact
+    void createNewContact()
+    {
+        cout << "Enter the first name: " ;
+        cin >> firstName;
+        cout << "Enter the last name: " ;
+        cin.ignore();
+        cin >> lastName;
+        cout << "Enter the phone number: ";
+        cin.ignore();
+        cin >> phoneNumber;
+        cout << "Enter the email: " ;
+        cin.ignore();
+        cin >> email;
+        cout << "Enter the notes: " ;
+        cin.ignore();
+        //cin >> notes;
+        getline(cin, notes);
+
+        cout << "\n";
+        //cout << "Contact created!";
+        //cout << endl;
+    }
+
+    // displays the contact
+    void printContact()
+    {
+        cout << "First Name: " << firstName << endl;
+        cout << "Last Name: " << lastName << endl;
+        cout << "Phone Number: " << phoneNumber << endl;
+        cout << "Email: " << email << endl;
+        cout << "Notes: " << notes << endl;
+    }
+
+    string getPhone()
+    {
+        return phoneNumber;
+    }
+    
+    string getName()
+    {
+        return firstName;
+    }
+
+    string getEmail()
+    {
+        return email;
+    }   
+
+    string getNotes()
+    {
+        return notes;
+    }  
+};
+
+contact cont; // contact object to reference the contact class
+
+// create a new contact and write it to a file
+void saveContact()
+{
+    datahandler.open("addressbook.txt", ios::out|ios::app); // open the file in append mode
+    cont.createNewContact(); // create a new contact
+    string address = firstName + "," + lastName + "," + phoneNumber + "," + email + "," + notes + "\n";
+    datahandler.write(address.data(), address.size()); // write the contact to the file
+    cout << address << endl;
+    datahandler.close(); // close the file
+
+    cout << endl <<"Contact has been added to the Addressbook" << endl;
+    //cout << endl << "Enter your choice: ";
+    //getchar();
+}
+
+// display all entries in the address book
+void displayAll()
+{
+   system("cls"); // clear the screen
+   cout << "    Displaying all contacts" << endl;
+   cout << "--------------------------------" << endl;
+   datahandler.open("addressbook.txt", ios::in); // open the file in read mode
+   vector<string> *g1; // create a vector of strings
+    g1 = new vector<string>[200]; 
+    if (datahandler.is_open()) 
+    {
+        string str; // string to store the line entries
+
+        // read the file line by line
+        while (getline(datahandler, str)){ 
+           stringstream linestream(str);
+           getline(linestream, firstName, ',');
+           getline(linestream, lastName, ',');
+           getline(linestream, phoneNumber, ',');
+           getline(linestream, email, ',');
+           getline(linestream, notes, ',');  
+
+           g1 -> push_back(str); // push the line into the vector
+           cont.printContact();
+           cout << "--------------------------------" << endl;
+           
+        }
+    datahandler.close(); // close the file
+    }
+    // Print the vector - Used for testing only (Comment out when not testing)
+    //cout << "\nVector elements are: ";
+    //for (auto it = g1->begin(); it != g1->end(); it++)
+    //    cout << *it << " ";
+}
+
+
+// Update a contact in the Addressbook
+void editContact()
+{
+    string name;
+    bool exists = false;
+    string fileContent;
+
+    cout << "Enter the first name of the contact you want to edit: ";
+    cin >> name;
+    
+    datahandler.open("addressbook.txt", ios::in|ios::in); // open the file in read mode
+    if (datahandler.is_open()) 
+    {
+        string str; // string to store the line entries
+
+        // read the file line by line
+        while (getline(datahandler, str))
+        { 
+           stringstream linestream(str);
+           getline(linestream, firstName, ',');
+           getline(linestream, lastName, ',');
+           getline(linestream, phoneNumber, ',');
+           getline(linestream, email, ',');
+           getline(linestream, notes, ',');  
+    
+            if(name == firstName) // if the name entered matches the name of the contact
+            {
+                cont.printContact(); // print the contact
+                cout << "--------------------------------" << endl;
+                cout << endl;
+                
+                cont.createNewContact(); // create a new contact
+                string address = firstName + "," + lastName + "," + phoneNumber + "," + email + "," + notes + "\n";                
+                fileContent += address; // add the new contact to the fileContent string
+
+                /*int pos = -1 * sizeof(cont); // get the position of the contact
+                datahandler.seekp(pos, ios::cur); // move the pointer to the position of the contact
+                
+                datahandler.write(address.data(), address.size()); // write the contact to the file
+                cout << address << " has been updated at " << pos << endl;*/
+
+                cout << "Contact has been updated in the Addressbook" << endl;
+                exists = true;
+            }
+            else
+            {
+                fileContent += str + "\n"; // add the line to the fileContent string
+            }
+        }
+    datahandler.close(); // close the file 
+    }
+    if (exists == false)
+    {
+        cout << endl << "Contact does not exist int the Addressbook" << endl;
+        getchar();
+    }
+    else
+    {
+        ofstream outFile("addressbook.txt"); // open the file in write mode
+        outFile << fileContent; // write the fileContent string to the file
+        outFile.close(); // close the file
+    }
+}
+
+// Delete a contact from the Addressbook
+void deleteContact()
+{
+    string name;
+    bool exists = false;
+
+    cout << endl << "Enter the first name of the contact you want to delete: ";
+    cin >> name;
+
+    datahandler.open("addressbook.txt", ios::in|ios::out); // open the file in read and write mode
+    fstream tempFile; // create a temporary file
+    tempFile.open("tempfile.txt", ios::out); // open the temporary file in write mode
+    datahandler.seekg(0, ios::beg); // move the pointer to the beginning of the file
+    
+    string str; // string to store the line entries
+    while (getline(datahandler, str)) // read the file record by record
+        {
+        stringstream linestream(str);
+        getline(linestream, firstName, ',');
+        getline(linestream, lastName, ',');
+        getline(linestream, phoneNumber, ',');
+        getline(linestream, email, ',');
+        getline(linestream, notes, ',');
+
+        if(name == cont.getName()) // if the name matches the name of the contact
+        {
+            cont.printContact(); // print the contact
+            cout << "--------------------------------" << endl;
+            cout << endl;
+            cout << "Contact deleted from Addressbook!" << endl;
+            exists = true;
+        }
+        else
+        {
+            // write the contact to the temporary file if it doesn't match the name entered
+            string address = firstName + "," + lastName + "," + phoneNumber + "," + email + "," + notes + "\n";
+            tempFile.write(address.data(), address.size()); // write the contact to the temporary file    
+        }
+    }
+    datahandler.close(); // close the file
+    tempFile.close(); // close the temporary file
+
+    remove("addressbook.txt"); // remove the original file
+
+    ifstream f1("tempfile.txt",ios::binary); // open the temporary file in read mode
+    ofstream f2("addressbook.txt",ios::binary); // open the original file in write mode
+    f2 << f1.rdbuf(); // copy the contents of the temporary file to the original file
+
+    f1.close(); // close the temporary file
+    f2.close(); // close the original file
+
+    remove("tempfile.txt"); // remove the temporary file
+
+    if (exists == false) // if the contact does not exist
+    {
+        cout << "Contact does not exist int the Addressbook" << endl;
+    }
+}
+
+// Main processing section
+
+int main(int argc, char *argv[])
+{
+    for(;;)
+    {
+        int choice;
+        cout << "Welcome to Chris' Addressbook" << endl;
+        cout << "-----------------------------" << endl;
+        cout << "1. Add a new contact" << endl;
+        cout << "2. Display all contacts" << endl;
+        cout << "3. Edit a contact" << endl;
+        cout << "4. Delete a contact" << endl;
+        cout << "5. Exit the Addressbook" << endl;
+        cout << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+                saveContact();
+                break;
+            case 2:
+                displayAll();
+                break;
+            case 3:
+                editContact();
+                break;
+            case 4:
+                deleteContact();
+                break;
+            case 5:
+                return 0;
+                break;
+            default:
+                cout << "Invalid choice, please try again" << endl;
+                break;
+        }
+
+        int option;
+        cout << "Do you want to continue? (1 = Yes, 2 = No): ";
+        cin >> option;
+        system("cls");
+
+        switch(option)
+        {
+        case 1:
+            continue;
+        case 2:
+            cout << endl << "Thank you for using Chris' Addressbook" << endl << endl;
+            return 0;
+            break;
+        default:
+            cout << "Invalid choice, Exiting the program!" << endl;
+            return 0;
+            break;
+            
+                
+        }   
+    }
+    return 0;
+}
+
